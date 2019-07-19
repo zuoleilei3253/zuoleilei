afd5c7ee981f25b3a2743fe784b6e8b678e56d39
zuoleilei3253 <32060498+zuoleilei3253@users.noreply.github.com>
2019-07-19 22:55:44 +0800

    Update config.py

--- mix_test/features/steps/config.py
+++ mix_test/features/steps/config.py
@@ -1,12 +1,12 @@
 # 设置邮箱
 MAIL_INFO = {
     'user': 'xxxx@126.com',
-    'password': 'xxxx.5',
+    'password': 'xxxxxx',
     'host': 'smtp.126.com',
     # 'smtp_ssl': True,  # 发件箱是qq邮箱的话，改成True
 }
 
-TO = ['leilei.zuo@vista.com']
+TO = ['xxx@vista.com']
 
 # 日志的配置
 log_level = 'info'
@@ -14,7 +14,7 @@ log_level = 'info'
 # 数据库配置
 host = 'xxx.com'
 port = 3306
-user = 'mix_rw'
+user = 'xxx'
 password = 'xxx'
 db = 'mix'
 charset = 'utf-8'
@@ -27,6 +27,6 @@ user_info = {"leilei": {"email": "xxx@163.com", "password": "xxx"}}
 HOSTS = {
     # 'QA': 'http://api.zhp.cn',  # 测试环境
     # 'DEV': 'http://dev.zhp.cn',  # 开发环境
-    'PRE': 'https://ap.xmp.one'  # 预生产环境
+    'PRE': 'https://xx.xmp.one'  # 预生产环境
 }
 domain = HOSTS.get('PRE')  # 默认测试环境的地址
